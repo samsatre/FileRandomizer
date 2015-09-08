68506b04f46bebcdd6481a11ccd6bc8f2ecd436c
SK207_24 <SK207_24@K207_W824.carrollcc.edu>
2015-09-08 19:32:15 -0400

    Writes to randomized file

--- FileRandomizer/src/cis232/lab/Randomizer.java
+++ FileRandomizer/src/cis232/lab/Randomizer.java
@@ -2,7 +2,9 @@ package cis232.lab;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Randomizer {
@@ -12,14 +14,19 @@ public class Randomizer {
 		File originalFile = new File("original_list.txt");
 		Scanner input = new Scanner(originalFile);
 		ArrayList<String> lines = new ArrayList<>();
+		Random rand = new Random();
+		PrintWriter write = new PrintWriter("randomized_list.txt");
 		
 		while(input.hasNextLine()){
 			lines.add(input.nextLine());
 		}
-
 		//For testing purposes, output all the lines
-		for(String line : lines){
-			System.out.println(line);
+		int length = lines.size();
+		while(length>0){
+			int index = rand.nextInt(length);
+			System.out.println(lines.get(index));
+			write.println(lines.remove(index));
+			length--;
 		}
 		
 		input.close();
